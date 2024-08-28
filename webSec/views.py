67e7b008fbae7cfe6b160398388c6067758c1b48
Rupak Pathak <pathakrupak00@gmail.com>
2024-08-28 06:51:49 +0545

    done

--- webSec/views.py
+++ webSec/views.py
@@ -1,4 +1,5 @@
 # myapp/views.py
+<<<<<<< Updated upstream
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -21,3 +22,44 @@ def land_page(request):
                 result_html = 'Invalid URL'
             return HttpResponse(f'Crawling result:<br>{result_html}')
     return render(request, 'index.html')
+=======
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def land_page(request):
+    return render(request, 'index.html')
+
+
+def Url_fetch(request):
+    return("url fetch")
+
+
+# def validate_url(url):
+#     validator = URLValidator()
+#     try:
+#         validator(url)
+#     except ValidationError:
+#         return False
+#     return True
+
+# def Get_input(request):
+#     if request.method == 'POST':
+#         link = request.POST.get('link', '')
+
+#         # Validate the URL
+#         if not validate_url(link):
+#             return render(request, 'myapp/results.html', {'result_message': 'Invalid URL format. Please enter a valid URL.'})
+
+#         # Assuming 'param_name' is the parameter to test for SQL injection
+#         param_name = 'query'  # This should be set to the actual parameter name you want to test
+
+#         # Run SQL Injection test
+#         results = test_sql_injection(link, param_name)
+
+#         # Combine results into a single message
+#         result_message = '<br>'.join(results)
+#         return render(request, 'myapp/results.html', {'result_message': result_message})
+
+#     return render(request, 'myapp/input_form.html')
+>>>>>>> Stashed changes
